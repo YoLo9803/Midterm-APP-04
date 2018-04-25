341e8134ba57cdfe62b8bae228a0b0eaa2627cc7
a105590011 <f98989000@gmail.com>
2018-04-25 18:11:25 +0800

    midterm  layout

---
+++ APP04_105590011/app/src/main/java/tw/edu/ntut/csie/app04_105590011/APP01_105590011.java
@@ -0,0 +1,13 @@
+package tw.edu.ntut.csie.app04_105590011;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class APP01_105590011 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app01_105590011);
+    }
+}
